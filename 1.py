5082e7f3efdd20f2159cb0de5d24e2478e1027b3
Covalscy <gartuyev20080512@mail.ru>
2026-01-15 11:24:39 +0300

    1

--- 1.py
+++ 1.py
@@ -1,3 +1,17 @@
 f = open("1.txt")
-f = f.readline().replace("."," .")
-print(f.split(" "))
+f = f.readline().replace(".", ". ")
+f = f.split(" ")
+k = 0
+a = []
+print(f)
+for i in range(len(f)):
+    p = 0
+    k = 0
+    for j in range(len(f)):
+        if 'Q' in f[j]:
+            k+=1
+        p+=len(f[j])
+        if k>=12000:
+            a.append(p)
+            break
+print(min(a))
