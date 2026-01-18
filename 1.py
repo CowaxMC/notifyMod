c7a7c29ce0b224a7179f4ce5c1f59eef2f119cc6
Covalscy <gartuyev20080512@mail.ru>
2026-01-18 03:35:11 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,17 +1,23 @@
 f = open("1.txt")
-f = f.readline().replace(".", ". ")
+f = f.readline()
+m = f
+f = f.replace(".", ". ")
 f = f.split(" ")
 k = 0
+d = []
 a = []
-print(f)
+p=0
 for i in range(len(f)):
+    p+=len(f[i])
+    if 'Q' in f[i]:
+        d.append(p)
+b = []
+l=11999
+for i in range(len(d)):
     p = 0
     k = 0
-    for j in range(len(f)):
-        if 'Q' in f[j]:
-            k+=1
-        p+=len(f[j])
-        if k>=12000:
-            a.append(p)
-            break
-print(min(a))
+    if i+l<len(d):
+        a.append(len(m[d[i]:d[i+l]]))
+
+print(min(a))  
+
